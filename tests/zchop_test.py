337fdf6d649c589ff2310e0247a83aa2f6fdb31f
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-14 16:12:22 -0500

    Support tuples, add tests

--- tests/zchop_test.py
+++ tests/zchop_test.py
@@ -3,7 +3,23 @@ from zchop import zchop
 
 def test_scalar():
     assert zchop(1.0) == 1.0
-    assert zchop(1e-20) == 0.0
+    assert zchop(1e-15) == 0.0
+    assert zchop(1) == 1
+    assert zchop(1, 3) == 0
+
+def test_complex():
+    z1 = 1 + 1e-15j
+    zz1 = 1 + 0j
+    assert zchop(z1) == zz1
+    z2 = 1e-15 + 1j
+    zz2 = 0.0 + 1j
+    assert zchop(z2) == zz2
+    za = [z1, z2]
+    zza = [zz1, zz2]
+    assert zchop(za) == zza
+    t = (za, za)
+    assert zchop(t) == (zza, zza)
+    assert zchop(za + za) == zza + zza
 
 def test_list():
     xin = [1, 2, 1e-15]
@@ -19,3 +35,8 @@ def test_complex_array():
     xin = numpy.array([1, 2, 1e-15 + 2.0j])
     xout = numpy.array([1, 2, 2.0j])
     assert numpy.alltrue(zchop(xin) == xout)
+
+def test_hetero():
+    assert zchop(["cat", 1e-15]) == ["cat", 0.0]
+    assert zchop(["cat", 1e-8]) == ["cat", 1e-8]
+    assert zchop(["cat", 1e-8, 1e-15], eps=1e-9) == ["cat", 1e-8, 0.0]
--- zchop.py
+++ zchop.py
@@ -12,7 +12,7 @@ def zchop(m, eps=_ZCHOP_EPS):
     less than eps, which is ``1e-12`` by default.
     If m is a complex number, then the real and imaginary parts are
     set to zero or not independently.
-    If m is a `list` or a numpy `array`, then structure is processed
+    If m is a `list`, `tuple`, or numpy `array`, then the structure is processed
     elementwise and the output type is equal to the input type.
     """
     if isinstance(m, float):
@@ -27,6 +27,8 @@ def zchop(m, eps=_ZCHOP_EPS):
         if m.dtype == 'float64':
             return _zchop_real_np(m)
         return _zchop_real_np(m.real) + 1j * _zchop_real_np(m.imag)
+    if isinstance(m, tuple):
+        return tuple(zchop(x) for x in m)
     if isinstance(m, int):
         if abs(m) < eps:
             return 0
